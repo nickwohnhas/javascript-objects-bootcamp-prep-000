035cb8ad62d2d80d4d5364890984221a1440d0af
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-08 01:28:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {Beck : "Odelay"}
 function updatePlaylist(playlist,artistName,songTitle){
 return Object.assign(playlist,{[artistName] : 'songTitle'})
 }
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName
+  return playlist
+}
